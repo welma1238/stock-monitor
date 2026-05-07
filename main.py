3b7bda7bfee93206d34d4b302b8ab2df6a39db80
welma1238 <welma1238@gmail.com>
2026-05-07 23:12:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from datetime import datetime
 # 從 Railway 系統抓取妳設定的變數
 TG_TOKEN = os.getenv("TG_TOKEN")
 TG_CHAT_ID = os.getenv("TG_CHAT_ID")
-YT_URL = "https://www.youtube.com/watch?v=WMT8VRPX-sM"
+YT_URL = "https://www.youtube.com/watch?v=oB2QY06L5Ew&t=896s"
 
 def run_task():
     # --- 診斷步驟 1：延遲啟動 ---
